30cdee0093db20a6c72d7f7cf196dfcdd571b6a2
Arjun-Ajai <pkd25cs015@gecskp.ac.in>
2026-07-04 21:59:55 +0530

    Fix<backtester>: signal_map lookup, NaN guard on final bar, iloc consistency

--- backtester.py
+++ backtester.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from strategy import *
+from strategy import Signal, BaseStrategy, SmaCrossStrategy
+import pandas as pd
 
 @dataclass
 class Trades:
@@ -22,16 +23,20 @@ class Backtester:
         equity_curve =[]
         cash=100000
         shares=0
+        signals_map={}
+        for s in signals:
+            signals_map[s.date]=s
         for i in range(len(df)):
-            for j in range(len(signals)):
-                if signals[j].date == df.date[i]:
-                    if signals[j].action == "BUY":
-                        shares = cash / df.next_open[i]
+            if df.date.iloc[i] in signals_map:
+                    if pd.isna(df.next_open.iloc[i]):
+                        continue
+                    if signals_map[df.date.iloc[i]].action == "BUY":
+                        shares = cash / df.next_open.iloc[i]
                         cash=0
                         trades.append(Trades(df.date.iloc[i],"BUY",df.next_open.iloc[i],shares,0,shares*df.next_open.iloc[i]))
 
-                    elif signals[j].action == "SELL":
-                        cash = shares * df.next_open[i]
+                    elif signals_map[df.date.iloc[i]].action == "SELL":
+                        cash = shares * df.next_open.iloc[i]
                         shares=0
                         trades.append(Trades(df.date.iloc[i],"SELL",df.next_open.iloc[i],0,cash,cash))
 
