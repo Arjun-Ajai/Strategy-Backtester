e7432b1ae8d9fa2a1a87b426f0dd34a3ed8b68bb
Arjun-Ajai <pkd25cs015@gecskp.ac.in>
2026-07-09 23:47:25 +0530

    feat(metrics): Add sharpe ratio to compute_metrics

--- metrics.py
+++ metrics.py
@@ -1,6 +1,16 @@
+import pandas as pd
 
-def compute_matrics(trades,equity,initial_value):
-    total_returns = ((trades.portfolio_value.iloc[-1] - initial_value) / initial_value) * 100
+
+def compute_metrics(trades,equity,initial_value):
+    total_returns = ((trades[-1].portfolio_value - initial_value) / initial_value) * 100
+    values = pd.Series([e.portfolio_value for e in equity])
+
+    daily_returns = values.pct_change()
+    sharpe = (daily_returns.mean() / daily_returns.std()) * (252 ** 0.5)
+
+    return {"total_return_pct": total_returns,"sharpe": sharpe}
+
+
 
 
 
