f75d769785f77bd9adc89a6d556808ce05c7ac8c
Arjun-Ajai <pkd25cs015@gecskp.ac.in>
2026-07-14 22:05:23 +0530

    feat(metrics):max drawdown, win rate, num trades

--- metrics.py
+++ metrics.py
@@ -8,7 +8,22 @@ def compute_metrics(trades,equity,initial_value):
     daily_returns = values.pct_change()
     sharpe = (daily_returns.mean() / daily_returns.std()) * (252 ** 0.5)
 
-    return {"total_return_pct": total_returns,"sharpe": sharpe}
+    peaks=values.cummax()
+    drawdown=(values-peaks)/peaks*100
+    max_drawdown_pct = min(drawdown)
+
+    win=0
+    buy_price=None
+    num_trades = 0
+    for t in trades:
+        if t.action == 'BUY':
+            buy_price=t.price
+        elif t.action == 'SELL' and buy_price is not None:
+            num_trades+=1
+            if buy_price<t.price:
+                win+=1
+    win_rate=win/num_trades*100
+    return {"total_return_pct": total_returns,"sharpe": sharpe,"max_drawdown_pct":max_drawdown_pct,"win_rate":win_rate,"num_trades":num_trades}
 
 
 
