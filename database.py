51922ac317ab4d5770771020b7e41f8a60a6bbf6
Arjun-Ajai <pkd25cs015@gecskp.ac.in>
2026-07-18 15:30:39 +0530

    feat(database):implement database wrapper with runs and trades persistence

--- database.py
+++ database.py
@@ -38,10 +38,10 @@ class Database:
         self.conn.commit()
 
     def save_runs(self, symbol, strategy, params, start_date, end_date,
-                  total_return, sharpe, max_drawdown, win_rate, num_trades, created_at):
+                  total_return, sharpe, max_drawdown, win_rate, num_trades):
         cursor = self.conn.execute("""INSERT INTO runs (symbol, strategy, params, start_date, end_date,
-        total_return, sharpe, max_drawdown, win_rate, num_trades, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
-        (symbol, strategy, params, start_date, end_date, total_return, sharpe, max_drawdown, win_rate, num_trades, created_at))
+        total_return, sharpe, max_drawdown, win_rate, num_trades) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+        (symbol, strategy, params, start_date, end_date, total_return, sharpe, max_drawdown, win_rate, num_trades))
         self.conn.commit()
         return cursor.lastrowid
 
@@ -51,4 +51,17 @@ class Database:
             (run_id, date, action, price, quantity, portfolio_value)
         )
         self.conn.commit()
-        return cursor.lastrowid
+
+    def save_equity(self,run_id,date,portfolio_value):
+        cursor = self.conn.execute("INSERT INTO equity_curve(run_id, date, portfolio_value) VALUES (?, ?, ?)",(run_id,date,portfolio_value))
+        self.conn.commit()
+
+    def get_all_runs(self):
+        cursor = self.conn.execute("SELECT * FROM runs ORDER BY sharpe DESC")
+        return cursor.fetchall()
+    def get_all_trades(self,run_id):
+        cursor=self.conn.execute("SELECT * FROM trades WHERE run_id=?",(run_id,))
+        return cursor.fetchall()
+    def get_all_equity(self,run_id):
+        cursor = self.conn.execute("SELECT * FROM equity_curve WHERE run_id=?",(run_id,))
+        return cursor.fetchall()
