36e12e5cdbe1ee4d727d6d80fd7b0e9f3559b238
Arjun-Ajai <pkd25cs015@gecskp.ac.in>
2026-07-20 21:50:40 +0530

    fix(visualize):fix field name and placement

--- visualize.py
+++ visualize.py
@@ -11,19 +11,19 @@ def plot_results(df_ohlcv, equity, trades, run_id, symbol):
     ax1.set_title('Price Chart')
     ax1.set_ylabel('Price($)')
     ax1.legend()
-    buy_dates=[trades.date for trades in trades if trades.action == 'BUY']
-    buy_prices=[trades.price for trades in trades if trades.action == 'BUY']
-    sell_dates=[trades.date for trades in trades if trades.action == 'SELL']
-    sell_prices=[trades.price for trades in trades if trades.action == 'SELL']
+    buy_dates=[trades.date for t in trades if trades.action == 'BUY']
+    buy_prices=[trades.price for t in trades if trades.action == 'BUY']
+    sell_dates=[trades.date for t in trades if trades.action == 'SELL']
+    sell_prices=[trades.price for t in trades if trades.action == 'SELL']
     ax1.scatter(buy_dates,buy_prices,marker='^',color='blue',label='BUY')
     ax1.scatter(sell_dates,sell_prices,marker='v',color='red',label='SELl')
     os.makedirs("results", exist_ok=True)
     plt.tight_layout()
     plt.savefig(f"results/run_{run_id}.png")
-    plt.close()
 
-    equity_dates=[equity.date for equity in equity ]
-    equity_values=[equity.price for equity in equity]
+
+    equity_dates=[equity.date for e in equity ]
+    equity_values=[equity.portfolio_value for e in equity]
     initial_shares = 100_000 / df_ohlcv.close.iloc[0]
     benchmark = (df_ohlcv.close * initial_shares).tolist()
     ax2.plot(equity_dates, equity_values, color='blue', label='Strategy')
@@ -31,6 +31,7 @@ def plot_results(df_ohlcv, equity, trades, run_id, symbol):
     ax2.set_title('Portfolio vs Buy & Hold')
     ax2.set_ylabel('Value ($)')
     ax2.legend()
+    plt.close()
 
 
 
